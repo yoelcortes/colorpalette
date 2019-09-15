785f2e100a7955ab3b1d62382c2832412b1b7115
yoelcortes <yoelcortes@gmail.com>
2019-09-15 02:52:19 -0500

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,9 @@ extensions = ['sphinx.ext.autodoc',
               'numpydoc',
               'nbsphinx']
 
+# Do not show all members of the class
+numpydoc_show_class_members = False
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -84,7 +87,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
